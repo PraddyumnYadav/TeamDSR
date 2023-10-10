2ab3b5b2e7e08029cedf385ea4a4a0c9c9c1e782
Praddyumn Yadav <praddyumny@gmail.com>
2023-10-10 22:24:17 +0530

    Setup Django Project.

---
+++ Class10th/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Class10ThConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Class10th'
---
+++ Class9th/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Class9ThConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Class9th'
