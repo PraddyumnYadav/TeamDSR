7066383ade18fa046692ce70705aa5206052699f
Praddyumn Yadav <praddyumny@gmail.com>
2023-10-21 23:22:28 +0530

    Make one entry Visible using django.

--- TeamDSR_Django/TeamDSR/views.py
+++ TeamDSR_Django/TeamDSR/views.py
@@ -6,7 +6,14 @@ def home(request):
 
 
 def class9(request):
-    return render(request, "class9.html")
+    notes = [
+        {
+            "name": "The French Revolution",
+            "pdf": "PDFs/Class 9th/The French Revolution one shot revision.pdf",
+            "thumbnail": "Images/class9/the_french_revolution.webp"
+        },
+    ]
+    return render(request, "class9.html", context=notes[0])
 
 
 def class10(request):
